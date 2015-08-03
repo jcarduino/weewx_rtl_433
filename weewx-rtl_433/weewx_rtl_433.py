bbf6315a4731ba54998e50408e7318720d543537
johan <jcarduino@corstjens.org>
2015-08-03 15:16:08 +0200

    first more or less working version. - detect sensors via rtl_433, - save data in dictionary, - write a file for importing in weexs using fileparse plugin
    much debug info written in syslog

--- weewx-rtl_433/weewx_rtl_433.py
+++ weewx-rtl_433/weewx_rtl_433.py
@@ -40,13 +40,24 @@ def printdebug(str):
 # Print data[]
 def print_data():
     global data
-    print "printing data"
+    global datafile
+    printdebug( "Open exportfile: " +datafile)
+    fo = open(datafile, "wb+")    
+    fo.write("Date="+ str(int(time.time()))+"\n")
+    printdebug("Date=" + str(int(time.time())))
+    fo.write("usUnits=17"+"\n") 
+    printdebug("usUnits=17")
     for x in data:
-        print (x),"=",  data[x]
+        if data[x]<>"":
+            vdata= str(x)+"="+data[x]+"\n"
+            fo.write(vdata)
+            printdebug(vdata)
+            data[x]=""
+    printdebug( "close file: " +datafile)
+    fo.close()
 # Get BMP180 data
 def get_bmp180():
     global data
-    data['Time'] = int(time.time())
     data['barometer']= str(float(sensor.read_pressure()/100))
     data['altimeter']= str(int( sensor.read_altitude()))
     data['pressure'] = str(float(sensor.read_sealevel_pressure()/100))
@@ -57,7 +68,7 @@ def get_bmp180():
 def process_data(msg):
     global data
     global rain
-    print ("Process data: "+msg)
+    printdebug ("Process data: "+msg)
     sline=msg.split()
     #AlectoV1 Wind Sensor 43: Wind speed 0 units = 0.00 m/s: Wind gust 0 units = 0.00 m/s: Direction 90 degrees: Battery OK
     if 'AlectoV1 Wind Sensor' in msg:
@@ -67,7 +78,7 @@ def process_data(msg):
         data['windDir']=sline[21]
         data['windSpeed']=sline[11]
         data['windGust']=sline[18]
-        print "Updated: wind data: "+ data['windDir']+" "+  data['windSpeed']+" "+  data['windGust'] 
+        printdebug( "Updated: wind data: "+ data['windDir']+" "+  data['windSpeed']+" "+  data['windGust'] )
     if 'LaCrosse TX Sensor' in msg:
                 #2015-07-02 06:37:34 LaCrosse TX Sensor 3c: Temperature 20.0 C / 68.0 F
                 #2015-07-02 12:22:37 LaCrosse TX Sensor 3f: Humidity 58.0%
@@ -75,43 +86,44 @@ def process_data(msg):
                 device=device.rstrip(':')
                 d_data=sline[7]
                 d_data=d_data.rstrip('%')
-                print ("LaCrosse TX Sensor")
+                printdebug ("LaCrosse TX Sensor")
                 #sensordata = (device,sline[6],d_data,'')
                 if device=="3f" and sline[6]=='Temperature':
                     data['outTemp']==d_data
-                    print "Updated: 3f outTemp: "+ data['outTemp']
+                    printdebug( "Updated: 3f outTemp: "+ data['outTemp'])
                 if device=="7e" and sline[6]=='Temperature':
                     data['inTemp']=d_data
-                    print "Updated: 7e inTemp: "+ data['inTemp']
+                    printdebug( "Updated: 7e inTemp: "+ data['inTemp'])
                 if device=="3f" and sline[6]=='Humidity':
                     data['out_Humidity']=d_data
-                    print "Updated: 3f outHum: "+ data['outHumidity']
+                    printdebug( "Updated: 3f outHum: "+ data['outHumidity'])
                     
 	#3f buiten 7e binnen hum temp
     if 'AlectoV1 Sensor' in msg:
         #2015-07-02 12:56:37 AlectoV1 Sensor 43 Channel 1: Temperature 29.3 C: Humidity 49 : Battery OK    
         device=sline[4]
         device=device.rstrip(':')
-        print("AlectoV1 Sensor")
+        printdebug("AlectoV1 Sensor")
         if device== "43" and sline[7]=='Temperature':#zolder
             data["extraTemp1"]=sline[8]
-            print "Updated: 43 extraTemp1: "+ data['extraTemp1']
+            printdebug( "Updated: 43 extraTemp1: "+ data['extraTemp1'])
         if device== "43" and sline[10]=='Humidity':#zolder
             data["extraHumid1"]=sline[11]
-            print "Updated: 43 extraHumid1: "+ data['extraHumid1']
+            printdebug( "Updated: 43 extraHumid1: "+ data['extraHumid1'])
         if device== "247" and sline[7]=='Temperature':#kelder
             data["extraTemp2"]=sline[8]
-            print "Updated: 247 extraTemp2: "+ data['extraTemp2']
+            printdebug( "Updated: 247 extraTemp2: "+ data['extraTemp2'])
         if device== "247" and sline[10]=='Humidity':#kelder
             data["extraHumid2"]=sline[11]
-            print "Updated: 247 extraHumid2: "+ data['extraHumid2'] 
+            printdebug( "Updated: 247 extraHumid2: "+ data['extraHumid2'] )
     if 'AlectoV1 Rain Sensor' in msg:
         #2015-07-02 12:23:42 AlectoV1 Rain Sensor 133: Rain 0.00 mm/m2: Battery OK
-        print "rain sensor!!!"
+        #NEEDS WORK!!! mm per unit (update interval)needs to be calculated 
+        printdebug ("rain sensor!!!")
         device=sline[5]
         device=device.rstrip(':')
-        data["rain"]=sline[7]
-        print "Updated: rain: "+ data['rain']
+        #data["rain"]=sline[7]
+        printdebug ("Updated(should be): rain: "+ data['rain'])
     
 #====================================
 #End data handler
@@ -122,19 +134,13 @@ def signal_handler(signal, frame):
         # sys.exit(0)
         #Continue exit process main loop
 
-def threaded_print_function():
-        global exportdata
-        print "starting pt"
-        while 1==1:
-            print "sleep for 5 secs"
-            time.sleep(5)
-            print "Printing....running"
-            exportdata=1
 #============================================
 if __name__ == '__main__':
     #capture control-c and kill signal
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
+    datafile="/var/tmp/datafile"#export file to write
+    interval_write_datafile=60# write file each 60 secs
     rain="0.0"
     debug=1
     printtime=time.time()
@@ -155,8 +161,8 @@ if __name__ == '__main__':
             process_data(stdout_queue.get())    
         # Sleep a bit before asking the readers again.
         time.sleep(.1)
-        if (time.time()-printtime)>10:
-            print(int(time.time()-printtime))
+        if (time.time()-printtime)>interval_write_datafile:
+            #print(int(time.time()-printtime))
             get_bmp180()
             print_data()
             printtime=time.time()
